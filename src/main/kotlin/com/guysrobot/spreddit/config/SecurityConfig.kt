0195961746099556b4ad8dd4155646aa95ff7513
GuysRGithub <guysdeveloperrobot@gmail.com>
2023-03-20 15:55:32 +0700

    Add security config

---
+++ src/main/kotlin/com/guysrobot/spreddit/config/SecurityConfig.kt
@@ -0,0 +1,31 @@
+package com.guysrobot.spreddit.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+import kotlin.jvm.Throws
+
+@Configuration
+class SecurityConfig : WebSecurityConfiguration() {
+    @Throws(Exception::class)
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http.authorizeHttpRequests { auth ->
+            auth.anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .cors(Customizer.withDefaults())
+                .oauth2ResourceServer()
+                .jwt()
+        }
+            .httpBasic(Customizer.withDefaults())
+
+        return http.build()
+    }
+}
